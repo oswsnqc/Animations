3eea351a90ccefe203dc324a6126fd38c412a3cd
Yisen Jing <jyisen@hotmail.com>
2016-05-13 21:37:30 -0700

    study animation for IOS system

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    var counter = 1
+    var timer = NSTimer()
+    var decision = true
+    
+    
 
+    @IBOutlet weak var girlimage: UIImageView!
+    @IBAction func updateimage(sender: AnyObject) {
+        if decision == true {
+            timer.invalidate()
+            decision = false
+        } else {
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+            decision = true
+        }
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+
+        
+    }
+    
+    func doAnimation() {
+        if counter == 5 {
+            counter = 1
+        } else {
+            counter++
+        }
+        
+        girlimage.image = UIImage(named: "frame\(counter).png")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /*
+    
+    override func viewDidLayoutSubviews() {
+        girlimage.frame = CGRectMake(100, 200, 0, 0)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(3, animations: { () -> Void in
+            self.girlimage.frame = CGRectMake(100, 200, 200, 200)
+            
+            
+            
+        })
+    }
+
+*/
 
 
 }
-
